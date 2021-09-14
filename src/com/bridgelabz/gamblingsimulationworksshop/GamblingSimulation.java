829036b7c00b3ed5251c270a0e6bf85d6af8fe62
govind6498 <govindsingh6498@gmail.com>
2021-09-14 15:29:51 +0530

    To Start with a Stake of 00 every day and bet  every game

--- src/com/bridgelabz/gamblingsimulationworksshop/GamblingSimulation.java
+++ src/com/bridgelabz/gamblingsimulationworksshop/GamblingSimulation.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.gamblingsimulationworksshop;
 
 public class GamblingSimulation {
+	public static final int STAKES_EVERYDAY = 100;
+	public static final int STAKES_EVERYGAME = 1;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation Game");
+
 	}
 }
