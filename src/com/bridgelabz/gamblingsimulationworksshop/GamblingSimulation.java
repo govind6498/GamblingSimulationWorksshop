54f950079acfff9af0fe4404e78c0edb165938ca
govind6498 <govindsingh6498@gmail.com>
2021-09-14 15:37:06 +0530

    Added:Gambler make  bet so either win or losse

--- src/com/bridgelabz/gamblingsimulationworksshop/GamblingSimulation.java
+++ src/com/bridgelabz/gamblingsimulationworksshop/GamblingSimulation.java
@@ -3,8 +3,25 @@ package com.bridgelabz.gamblingsimulationworksshop;
 public class GamblingSimulation {
 	public static final int STAKES_EVERYDAY = 100;
 	public static final int STAKES_EVERYGAME = 1;
-	public static void main(String[] args) {
-		System.out.println("Welcome to Gambling Simulation Game");
+	public static final int WIN = 1;
+	public static final int LOSE = 0;
 
+	public static int everyGameBetOutcome(int cash) {
+		int gameState = (int)Math.floor(Math.random()*10)%2;
+		if(gameState == WIN) {
+			cash++;
+		}
+		else if(gameState == LOSE) {
+			cash--;
+		}
+		return cash;
 	}
+
+	public static void main(String[] args) {
+
+		int netCash = STAKES_EVERYDAY;
+		netCash = everyGameBetOutcome(netCash);
+		System.out.println(netCash);
+	}
+
 }
