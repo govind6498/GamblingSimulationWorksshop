ebb497b56efb447ca66c585d26aaa23595ad566e
govind6498 <govindsingh6498@gmail.com>
2021-09-14 15:25:22 +0530

    Added:Welcome to Gambling Simulation Game

---
+++ src/com/bridgelabz/gamblingsimulationworksshop/GamblingSimulation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblingsimulationworksshop;
+
+public class GamblingSimulation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation Game");
+	}
+}
